fa61999531e6d8860dff310f96adb6a2536fc7de
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-10 14:56:10 +0300

    ArrayList folder added

---
+++ ArrayList/ArrayList/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnOk = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'BtnOk
+        '
+        Me.BtnOk.Location = New System.Drawing.Point(163, 284)
+        Me.BtnOk.Name = "BtnOk"
+        Me.BtnOk.Size = New System.Drawing.Size(175, 47)
+        Me.BtnOk.TabIndex = 0
+        Me.BtnOk.Text = "Ok"
+        Me.BtnOk.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(111, 92)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(311, 160)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BtnOk)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnOk As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ ArrayList/ArrayList/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub BtnOk_Click(sender As Object, e As EventArgs) Handles BtnOk.Click
+        Dim totalNumber As String = "Sayıların Toplamı : "
+        Dim x(2), total As Integer
+        x(0) = 5
+        x(1) = 9
+        x(2) = 8
+        total = x(0) + x(1) + x(2)
+
+        Dim str(2) As String
+        str(0) = "hello"
+        str(1) = "world"
+        str(2) = "!"
+        ListBox1.Items.Add(x(0))
+        ListBox1.Items.Add(x(1))
+        ListBox1.Items.Add(x(2))
+        ListBox1.Items.Add(totalNumber)
+        ListBox1.Items.Add(total)
+        ListBox1.Items.Add(str(0))
+        ListBox1.Items.Add(str(1))
+        ListBox1.Items.Add(str(2))
+
+        'comment line
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+End Class
