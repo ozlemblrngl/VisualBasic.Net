fe20f281ef8b8b4fa34a4900e72698084ebc0f8d
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-10 22:50:03 +0300

    arraylist two dimension folder added

---
+++ ArrayListTwoDimension/ArrayListTwoDimension/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub BtnOk_Click(sender As Object, e As EventArgs) Handles BtnOk.Click
+        Dim name(2, 2), str As String
+        name(0, 0) = "ankara"
+        name(0, 1) = "istanbul"
+        name(0, 2) = "izmir"
+        name(1, 0) = "adana"
+        name(1, 1) = "çorum"
+        name(1, 2) = "malatya"
+        name(2, 0) = " samsun"
+        name(2, 1) = "burdur"
+        name(2, 2) = "erzurum"
+
+        str = name(0, 0) + " ve " + name(0, 1)
+        ListBox1.Items.Add(str)
+
+
+        'first row, second columns, we have 9 elements
+
+    End Sub
+End Class
