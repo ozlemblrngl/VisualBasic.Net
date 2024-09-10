5b7a044a4b251837e4d3d010ab5831f81354a7ec
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-11 00:00:21 +0300

    LoopForEach Folder Added

---
+++ LoopForEach/LoopForEach/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnOk = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BtnTry = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnOk
+        '
+        Me.BtnOk.Location = New System.Drawing.Point(52, 224)
+        Me.BtnOk.Name = "BtnOk"
+        Me.BtnOk.Size = New System.Drawing.Size(240, 103)
+        Me.BtnOk.TabIndex = 0
+        Me.BtnOk.Text = "Ok"
+        Me.BtnOk.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(135, 31)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(397, 160)
+        Me.ListBox1.TabIndex = 1
+        '
+        'BtnTry
+        '
+        Me.BtnTry.Location = New System.Drawing.Point(377, 224)
+        Me.BtnTry.Name = "BtnTry"
+        Me.BtnTry.Size = New System.Drawing.Size(212, 103)
+        Me.BtnTry.TabIndex = 2
+        Me.BtnTry.Text = "Try"
+        Me.BtnTry.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnTry)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BtnOk)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnOk As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BtnTry As Button
+End Class
---
+++ LoopForEach/LoopForEach/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub BtnOk_Click(sender As Object, e As EventArgs) Handles BtnOk.Click
+        ListBox1.Items.Clear()
+
+        Dim name(2, 2), str As String
+        name(0, 0) = "Ankara"
+        name(0, 1) = "istanbul"
+        name(0, 2) = "izmir"
+        name(1, 0) = "adana"
+        name(1, 1) = "malatya"
+        name(1, 2) = "samsun"
+        name(2, 0) = "çorum"
+        name(2, 1) = "antalya"
+        name(2, 2) = "bilecik"
+
+        For Each str In name
+            ListBox1.Items.Add(str)
+        Next
+    End Sub
+
+    Private Sub BtnTry_Click(sender As Object, e As EventArgs) Handles BtnTry.Click
+
+        ListBox1.Items.Clear()
+        Dim i, y(2) As Integer
+        y = {2, 9, 7}
+
+        For Each i In y
+            ListBox1.Items.Add(i)
+        Next
+    End Sub
+End Class
