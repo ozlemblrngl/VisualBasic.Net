325f09fd60a3ba2bd1594ba86d0f774f8de62fc8
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-11 16:39:35 +0300

    Conditions Folder Added

---
+++ Conditions/Conditions/Program.vb
@@ -0,0 +1,15 @@
+Module Program
+    Sub Main(args As String())
+
+        'True / False
+        '(4<5) ...T
+        '(3>6) ...F
+
+        'Cond_1  Cond_2   Or   And
+        'T       T        T    T
+        'T       F        T    F
+        'F       T        T    F
+        'F       F        F    F 
+
+    End Sub
+End Module
